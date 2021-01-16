83e3d4eff553e3ce33c93344e6348de9b08d4c66
maciekbaczmanski <cakeisalie16@gmail.com>
2021-01-16 14:25:45 +0100

    communication

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 import paho.mqtt.client as mqtt  # import the client1
-
+import diodes
 
 ############
 def on_message(client, userdata, message):
     global messagereceived, message_str
     print("message received ", str(message.payload.decode("utf-8")))
     print("message topic=", message.topic)
+    diodes.charge_to_diode(str(message.payload.decode("utf-8")))
+    client.publish("trumba/charge", ("Received charge: ",str(message.payload.decode("utf-8"))))
 
 
 ########################################
 broker_address = "192.168.0.180"
-client = mqtt.Client("Reciever")  # create new instance
+client = mqtt.Client("receiver_trumba")  # create new instance
 client.on_message = on_message  # Przerwanie przy dostaniu wiadomosci
 client.connect(broker_address)
 client.loop_start()  # start the loop
-client.subscribe("Akcelerometr")  # subskrypcja
+client.subscribe("trumba/charge")  # subskrypcja
 input("press anything to stop")
-client.loop_stop()  # stop the loop
 
-client.publish("Akcelerometr", "ItsWorkingGuys")
+
+# client.publish("trumba/charge", "ItsWorkingGuys")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.loop_stop()  # stop the loop
 client.disconnect(broker_address)
