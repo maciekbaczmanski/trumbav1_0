fbdd83d2f75bfc6ae5252f2337ef372a4cbc9ae7
balbinamolerus <bmolerus@student.agh.edu.pl>
2021-01-16 22:44:51 +0100

    distance

--- distance.py
+++ distance.py
@@ -4,22 +4,22 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-print "Measuing Distance"
-print "Press ctrl +c to stop me"
-GPIO.setup(23,GPIO.OUT)
-GPIO.setup(24,GPIO.IN)
+print("Measuing Distance")
+print("Press ctrl +c to stop me")
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.IN)
 time.sleep(0.02)
-GPIO.output(23,False)
-print "Setting Trigger pin to zero by default"
+GPIO.output(23, False)
+print("Setting Trigger pin to zero by default")
 time.sleep(1)
 while True:
-        GPIO.output(23,True)
-        time.sleep(0.00001)
-        GPIO.output(23,False)
-        while GPIO.input(24)==0:
-                start_time=time.time()
-        while GPIO.input(24)==1:
-                end_time=time.time()
-        time=end_time-start_time
-        distance=17150*time
-        print "Measured Distance is:", distance ,"cms."
+    GPIO.output(23, True)
+    time.sleep(0.00001)
+    GPIO.output(23, False)
+    while GPIO.input(24) == 0:
+        start_time = time.time()
+    while GPIO.input(24) == 1:
+        end_time = time.time()
+    time = end_time - start_time
+    distance = 17150 * time
+    print("Measured Distance is:", distance, "cms.")
