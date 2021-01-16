83834ffceca46da3c9888efbd327f611ab0c106b
maciekbaczmanski <cakeisalie16@gmail.com>
2021-01-16 16:13:55 +0100

    pwm

--- PPM.py
+++ PPM.py
@@ -5,26 +5,27 @@ servoPIN = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 500) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+p = GPIO.PWM(servoPIN, 500)  # GPIO 17 for PWM with 50Hz
+p.start(2.5)  # Initialization
 try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+    while True:
+        x = input("predkosc")
+        p.ChangeDutyCycle(x)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(7.5)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(10)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(12.5)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(10)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(7.5)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(5)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(2.5)
+        # time.sleep(0.5)
 except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+    p.stop()
+    GPIO.cleanup()
