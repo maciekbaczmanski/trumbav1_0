6e120239bc43790e257a561080acd7a0018d602a
maciekbaczmanski <cakeisalie16@gmail.com>
2021-01-17 00:40:16 +0100

    battery

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ while battery_power:
     diodes.charge_to_diode(battery_power.power)
     message_to_send = str(battery_power.power)
     client.publish("trumba/power", message_to_send)
-    diodes.change_a_speed(battery_power.power)
-    diodes.change_b_speed(battery_power.power)
+    diodes.change_a_speed(99)
+    diodes.change_b_speed(99)
     if distance_measure.dist < 30:
         diodes.change_a_speed(0)
         diodes.change_b_speed(0)
