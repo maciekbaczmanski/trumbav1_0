b570d404987e9d63b54979a904d90094d1f95de7
maciekbaczmanski <cakeisalie16@gmail.com>
2021-01-17 11:06:04 +0100

    motor_pins

--- distance.py
+++ distance.py
@@ -12,16 +12,16 @@ class distance:
 
     def run(self):
         while self._running:
-
-            GPIO.output(23, True)
-            time.sleep(0.00001)
-            GPIO.output(23, False)
-            while GPIO.input(24) == 0:
-                start_time = time.time()
-            while GPIO.input(24) == 1:
-                end_time = time.time()
-            time.sleep(0.1)
             try:
+                GPIO.output(23, True)
+                time.sleep(0.00001)
+                GPIO.output(23, False)
+                while GPIO.input(24) == 0:
+                    start_time = time.time()
+                while GPIO.input(24) == 1:
+                    end_time = time.time()
+                time.sleep(0.1)
+
                 time1 = end_time - start_time
                 self.dist = 17150 * time1
             except:
