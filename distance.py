32b3f2028d341f82e1c6295bfdd376f7b9143612
maciekbaczmanski <cakeisalie16@gmail.com>
2021-01-17 11:17:52 +0100

    motor_pins

--- distance.py
+++ distance.py
@@ -20,22 +20,26 @@ class distance:
             lastinp = GPIO.input(24)
             testtime = time.time()
             error = False
-            while measurement:
-                inp = GPIO.input(24)
+            # while measurement:
+            #     inp = GPIO.input(24)
+            #
+            #     if inp and not lastinp:
+            #         start_time = time.time()
+            #
+            #     elif lastinp and not inp:
+            #         end_time = time.time()
+            #         measurement = False
+            #     lastinp = inp
+            while GPIO.input(24) == 0:
+                start_time = time.time()
                 if abs(testtime - time.time()) > 0.1:
                     error = True
                     break
-                if inp and not lastinp:
-                    start_time = time.time()
-
-                elif lastinp and not inp:
-                    end_time = time.time()
-                    measurement = False
-                lastinp = inp
-            # while GPIO.input(24) == 0:
-            #     start_time = time.time()
-            # while GPIO.input(24) == 1:
-            #     end_time = time.time()
+            while GPIO.input(24) == 1:
+                end_time = time.time()
+                if abs(testtime - time.time()) > 0.1:
+                    error = True
+                    break
             time.sleep(0.1)
             if not error:
                 time1 = end_time - start_time
