cca996c51f62525a634a748be982c0eba9347717
maciekbaczmanski <30316496+maciekbaczmanski@users.noreply.github.com>
2021-02-15 09:05:41 +0100

    Update Send.py
    added QOS & retain options for publishing

--- Send.py
+++ Send.py
@@ -3,5 +3,5 @@ broker_address="192.168.0.180"
 client = mqtt.Client()
 client.username_pw_set(“username”, “password”)
 client.connect(broker_address)
-client.publish("Akcelerometr","HelloThere")
+client.publish("Akcelerometr","HelloThere",qos=1,retain=True)
 client.disconnect(broker_address)
