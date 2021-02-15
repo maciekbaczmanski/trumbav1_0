38f8e32a410ac5300c08668908d4346e6ceec562
maciekbaczmanski <30316496+maciekbaczmanski@users.noreply.github.com>
2021-02-15 09:02:15 +0100

    Update Send.py
    added mqtt authentication

--- Send.py
+++ Send.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt #import the client1
 broker_address="192.168.0.180"
-client = mqtt.Client("Sender")
+client = mqtt.Client()
+client.username_pw_set(“username”, “password”)
 client.connect(broker_address)
 client.publish("Akcelerometr","HelloThere")
 client.disconnect(broker_address)
